c70572e5f5808d975e63fe12a8948afaaf71a587
jamie <j2clark@gmail.com>
2024-04-24 21:32:20 -0700

    fix buildspec update lambda definition

--- code/terraform/lambda.tf
+++ code/terraform/lambda.tf
@@ -5,10 +5,12 @@ data "archive_file" "lambda_package" {
 }
 
 resource "aws_lambda_function" "html_lambda" {
+  function_name = "${var.project_name}-${var.branch}-helloworld"
   filename = "index.zip"
-  function_name = "myLambdaFunction"
   role = data.aws_iam_role.execution_role.arn
   handler = "index.handler"
   runtime = "nodejs18.x"
   source_code_hash = data.archive_file.lambda_package.output_base64sha256
+
+  tags = local.common_tags
 }
