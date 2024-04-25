8d67ecbf203cbe6f04b9389cafc948b10f46ef18
jamie <j2clark@gmail.com>
2024-04-24 22:05:37 -0700

    add url

--- code/terraform/lambda_functionurl.tf
+++ code/terraform/lambda_functionurl.tf
@@ -11,9 +11,27 @@ resource "aws_lambda_function" "lambda_functionurl" {
   handler = "index.handler"
   runtime = "nodejs18.x"
   source_code_hash = data.archive_file.lambda_functionurl_package.output_base64sha256
+  publish = true
 
   tags = local.common_tags
 }
+
+resource "aws_lambda_function_url" "functionurl" {
+  function_name      = aws_lambda_function.lambda_functionurl.function_name
+  qualifier          = "$LATEST"
+  authorization_type = "NONE"
+  /*cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET,POST"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }*/
+}
+
+
+
 
 output "lambda_functionurl" {
   value = aws_lambda_function.lambda_functionurl.function_name
